117d1e0c9e8c437991aa2c9d2cb64b2b9a1238e1
amachkel <amachkel@gmail.com>
2022-03-28 14:12:32 -0400

    added tag routes

--- models/index.js
+++ models/index.js
@@ -11,12 +11,18 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
-  onDelete: 'SET NULL',
+  onDelete: "SET NULL",
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, {
+  through: { model: ProductTag, unique: false },
+  as: "product_tags",
+});
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, {
+  through: { model: ProductTag, unique: false },
+  as: "tags_for_products",
+});
 
 module.exports = {
   Product,
